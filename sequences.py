95c63b7b41d30a3d28d360abc74b826a3fae17bf
Ryuunosuke <88852555+Ryuunosuk3@users.noreply.github.com>
2022-02-15 14:08:11 +0100

    Add files via upload

---
+++ sequences.py
@@ -0,0 +1,8 @@
+
+def suites(n: int, var_suite: str, nb_rpt: int):
+    print(f'u0 =', n, f'({var_suite})')
+    for i in range(nb_rpt):
+        print(f'u{i+1} =', int(eval(var_suite)))
+        n = int(eval(var_suite))
+
+suites(10, '100 + n * 10', 25)
