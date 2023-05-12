d63670e0229f9fc6a66347e8a8205ab10cc98c95
Ryuunosuke <88852555+Ryuunosuk3@users.noreply.github.com>
2023-05-12 04:54:40 +0200

    Update series.py

--- series.py
+++ series.py
@@ -3,6 +3,4 @@ def series(n: int, var_suite: str, nb_rpt: int):
     print(f'u0 =', n, f'({var_suite})')
     for i in range(nb_rpt):
         print(f'u{i+1} =', int(eval(var_suite)))
-        n = int(eval(var_suite))
-
-series(10, '100 + n * 10', 25)
+        n = int(eval(var_suite))
